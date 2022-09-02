af1b29d7d8462de07b282ae0821f05cddfff6951
Obi Fortune <gabrielobi.of@gmail.com>
2022-09-02 13:33:17 +0300

    added id to the refresh chats

--- hooks/useRefreshChats.ts
+++ hooks/useRefreshChats.ts
@@ -2,9 +2,15 @@ import { SetChats } from '>context/actionCreators';
 import { appContext } from '>context/store';
 import { Chat } from '>types/chat';
 import { firestore } from '>utils/firebase';
-import { collection, query, where } from 'firebase/firestore';
+import {
+  collection,
+  DocumentData,
+  query,
+  QuerySnapshot,
+  where,
+} from 'firebase/firestore';
 import { useContext, useEffect } from 'react';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 interface I {
   id: Chat['senderID'];
@@ -12,13 +18,22 @@ interface I {
 const useRefreshChats = ({ id }: I) => {
   const { dispatch } = useContext(appContext);
 
-  const [values, loading, error] = useCollectionData(
+  const [values, loading, error] = useCollection(
     query(collection(firestore, 'chats'), where('senderID', '==', id)),
   );
 
+  const handleChange = <T>(coll: QuerySnapshot<DocumentData>) => {
+    const chats = coll.docs.map(doc => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+
+    return chats as unknown as T;
+  };
+
   useEffect(() => {
-    if (!loading && !error) {
-      dispatch(SetChats(values as Chat[]));
+    if (!loading && !error && values) {
+      dispatch(SetChats(handleChange<Chat[]>(values)));
     }
   }, [values, loading, error, dispatch]);
 
